98d94abce43c756ef46adcde85b684fb1fd5d5f3
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2023-12-12 15:47:10 -0400

    commit

--- tela_cliente.py
+++ tela_cliente.py
@@ -16,23 +16,26 @@ def menu_cliente():
     """
 
     while True:
-        menu = print("""
+        menu = input("""
 *********************************************************
     _________________ LOGIN CLIENTE _________________
 
         Digite [1] para cliente já logado
         Digite [2] para cadastrar login do cliente
         
-********************************************************* """)
+********************************************************* 
+
+>>> Digite a opção: """)
         if menu == '1':
             # Cliente informa que possui cadastro, então digita o CPF para verificarmos se é existente no banco de dados, se não, ele recebe a mensagem de CPF inexistente
-            cpf_cliente = input('\n >>> Informe seu CPF: \n')
-            if cpf_cliente ...
+            cpf_cliente = input('\n >>> Informe seu CPF: ')
+            if cpf_cliente:
+                pass
                 
             else:
                 print('\n - CPF INVÁLIDO!!! - \n')
         elif menu == '2':
-            # Ao informar que o cliente não possui cadastro, ele é redirecionado para a função 'menu_cadastro_cliente'
+            # Ao informar que o cliente não possui cadastro, ele é redirecionado para a função 'menu_cadastro_cliente' para realizar seu cadastro
             menu_cadastro_cliente()
         else:
             print('\n - OPÇÃO INVÁLIDA!!! - \n')
@@ -46,27 +49,27 @@ def menu_cadastro_cliente():
     """
 
     while True:
-        menu = print("""
+        menu = input("""
 *********************************************************
     __________________ CADASTRE-SE __________________
 
-        Digite [1] para voltar ao menu principal
+        Digite [1] para voltar ao menu de login
         Digite [2] para continuar ao cadastro de cliente
         
-********************************************************* """)
+********************************************************* 
+
+>>> Digite a opção: """)
         if menu == '1':
-            print('\n - VOLTANDO AO MENU PRINCIPAL!!! - \n')
+            print('\n - VOLTANDO AO MENU DE LOGIN!!! - \n')
             break
         elif menu == '2':
-            continue
+            cpf_cliente = input('Digite seu CPF: ')
+            email_cliente = input('Digite seu email: ')
+            nome_cliente = input('Digite seu nome completo: ') 
+            telefone_cliente = input('Digite seu telefone: ')
+            print(f'\n - CLIENTE > {nome_cliente} < CADASTRADO - \n')
         else:
             print('\n - OPÇÃO INVÁLIDA!!! - \n')
-
-        cpf_cliente = input('Digite seu CPF: ')
-        email_cliente = input('Digite seu email: ')
-        nome_cliente = input('Digite seu nome completo: ') 
-        telefone_cliente = input('Digite seu telefone: ')
-        print(f' - CLIENTE > {nome_cliente} < CADASTRADO - ')
 
 def menu_solicitacao_cliente():
     """
