3b1d8e0193c7c2ef9c50662e756964e525e91736
Pastrello <125152977+rafllosilva@users.noreply.github.com>
2023-12-12 15:10:28 -0400

    commit

--- tela_cliente.py
+++ tela_cliente.py
@@ -1,4 +1,5 @@
 import sqlite3
+import solicitacoes
 
 # Cria uma conexão com o banco de dados
 conexao_db = sqlite3.connect('cyber_solucoes.db')
@@ -7,6 +8,64 @@ conexao_db = sqlite3.connect('cyber_solucoes.db')
 cursor = conexao_db.cursor()
 
 def menu_cliente():
+    """
+    - Função para verificar se o cliente já está logado ou não, se não estiver logado, ele é redirecionado à função 'menu_cadastro_cliente';
+    - Não recebe parâmetros;
+    - Exemplo de uso:
+    >>> menu_cliente():
+    """
+
+    while True:
+        menu = print("""
+*********************************************************
+    _________________ LOGIN CLIENTE _________________
+
+        Digite [1] para cliente já logado
+        Digite [2] para cadastrar login do cliente
+        
+********************************************************* """)
+        if menu == '1':
+            # Cliente informa que possui cadastro, então digita o CPF para verificarmos se é existente no banco de dados, se não, ele recebe a mensagem de CPF inexistente
+            print('\n >>> Informe seu CPF: \n')
+            break
+        elif menu == '2':
+            # Ao informar que o cliente não possui cadastro, ele é redirecionado para a função 'menu_cadastro_cliente'
+            menu_cadastro_cliente()
+        else:
+            print('\n - CPF INEXISTENTE!!! - \n')
+
+def menu_cadastro_cliente():
+    """
+    - Função para cadastrar cliente;
+    - Não recebe parâmetros;
+    - Exemplo de uso:
+    >>> menu_cadastro_cliente():
+    """
+
+    while True:
+        menu = print("""
+*********************************************************
+    __________________ CADASTRE-SE __________________
+
+        Digite [1] para voltar ao menu principal
+        Digite [2] para continuar ao cadastro de cliente
+        
+********************************************************* """)
+        if menu == '1':
+            print('\n - VOLTANDO AO MENU PRINCIPAL!!! - \n')
+            break
+        elif menu == '2':
+            continue
+        else:
+            print('\n - OPÇÃO INVÁLIDA!!! - \n')
+
+        cpf_cliente = input('Digite seu CPF: ')
+        email_cliente = input('Digite seu email: ')
+        nome_cliente = input('Digite seu nome completo: ') 
+        telefone_cliente = input('Digite seu telefone: ')
+        print(f' - CLIENTE > {nome_cliente} < CADASTRADO - ')
+
+def menu_solicitacao_cliente():
     """
     - Função para exibir o menu principal do arquivo, que possui opções de : [v] Voltar ao menu principal, [1] , [2] , [3] ;
     - Não recebe parâmetros;
@@ -20,8 +79,8 @@ def menu_cliente():
     _____________________ OPÇÕES ____________________
 
     [v] .................... Voltar ao menu principal
-    [1] ........................... Enviar reclamação
-    [2] ........................ Consultar reclamação
+    [1] .......................... Enviar solicitação
+    [2] ....................... Consultar solicitação
 *********************************************************
 
 >>> Digite a opção: """)
@@ -29,18 +88,15 @@ def menu_cliente():
             print('\n - VOLTANDO AO MENU PRINCIPAL!!! - \n')
             break
         elif opcao == '1':
-            print('\n -  - \n')
-            enviar_reclamacao()
+            print('\n - ENVIAR SOLICITAÇÃO - \n')
+            solicitacoes.enviar_solicitacao()
         elif opcao == '2':
-            print('\n -  - \n')
-            consultar_reclamacao()
+            print('\n - CONSULTAR SLICITAÇÃO - \n')
+            consultar_solicitacao()
         else:
             print('\n - OPÇÃO INVÁLIDA!!! - \n')
 
-def enviar_reclamacao():
-    pass
-
-def consultar_reclamacao():
+def consultar_solicitacao():
     pass
 
 if __name__ == '__main__':
